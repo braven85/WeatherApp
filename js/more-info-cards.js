125a4011037505d2c13a0af2466f19a4388df4bd
Radek <radekie@wp.pl>
2022-01-30 19:46:12 +0100

    swipe v1.0

--- js/more-info-cards.js
+++ js/more-info-cards.js
@@ -206,6 +206,65 @@ moreInfoArrowLeft.onclick = function () {
   })
 }
 
+moreInfoCardsContainer.addEventListener('touchstart', handleTouchStart, false);
+moreInfoCardsContainer.addEventListener('touchmove', handleTouchMove, false);
+
+let xDown = null;
+let yDown = null;
+
+function handleTouchStart(evt) {
+  xDown = evt.touches[0].clientX;
+  yDown = evt.touches[0].clientY;
+}
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+  if (Math.abs(xDiff) + Math.abs(yDiff) > 150) { //to deal with to short swipes
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+      /*most significant*/
+      if (xDiff > 0) {
+        /* left swipe */
+        // alert('left!');
+        scrollAmount = 0;
+        moreInfoCardsContainer.scrollBy({
+          left: +130,
+          behavior: 'smooth'
+        })
+      } else {
+        /* right swipe */
+        // alert('right!');
+        scrollAmount = 0;
+        moreInfoCardsContainer.scrollBy({
+          left: -130,
+          behavior: 'smooth'
+        })
+      }
+    } else {
+      if (yDiff > 0) {
+        /* up swipe */
+        // alert('Up!');
+      } else {
+        /* down swipe */
+        // alert('Down!');
+      }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+  }
+}
+
+
+
 export {
   getDataForMoreInfo
 };
